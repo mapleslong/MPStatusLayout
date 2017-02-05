c20c03a3d3c25bb943e9ccdb5239abb4656d1402
mapleslong <mapleslong@gmail.com>
2017-02-05 13:40:42 +0800

    重新建立实现模式

---
+++ app/src/main/java/com/mapleslong/mpstatuslayoutdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mapleslong.mpstatuslayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapleslong.widget.MPStatusLayout;
+
+public class MainActivity extends AppCompatActivity {
+    MPStatusLayout mStatusLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mStatusLayout= (MPStatusLayout) findViewById(R.id.status_layout);
+        mStatusLayout.setStatus(MPStatusLayout.STATUS_ERROR);
+    }
+}
