d355bc10d7d91b11420737c052b2d1fbe28d53cc
mapleslong <mapleslong@gmail.com>
2017-03-06 11:08:29 +0800

    [MOD]更改布局新增版本号

--- app/src/main/java/com/mapleslong/mpstatuslayoutdemo/MainActivity.java
+++ app/src/main/java/com/mapleslong/mpstatuslayoutdemo/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mStatusLayout= (MPStatusLayout) findViewById(R.id.status_layout);
-        mStatusLayout.setStatus(MPStatusLayout.STATUS_EMPTY);
+        mStatusLayout.setStatus(MPStatusLayout.STATUS_NONETWORK);
     }
 }
